de9dd0af9697715423df665d125e821393e5b1aa
ToonyStank <hakeemiqbalnew@gmail.com>
2021-10-23 22:48:51 +0530

    EventHandler onRightClick

--- src/main/java/com/toonystank/armorswap/Events/onRightClick.java
+++ src/main/java/com/toonystank/armorswap/Events/onRightClick.java
@@ -1,7 +1,53 @@
 package com.toonystank.armorswap.Events;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Objects;
 
 public class onRightClick implements Listener {
 
+    @EventHandler
+    public void onRightClickEvent(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (Objects.equals(event.getItem(), player.getInventory().getItemInMainHand())) {
+            if (event.getAction() == Action.RIGHT_CLICK_AIR) {
+                ItemStack Item = player.getInventory().getItemInMainHand();
+                if (Item.getType().toString().toLowerCase().contains("helmet")) {
+                    ItemStack returnItem = player.getInventory().getHelmet();
+                    player.getInventory().setItemInMainHand(returnItem);
+                    player.getInventory().setHelmet(Item);
+                    player.playSound(player.getLocation(), Sound.ITEM_ARMOR_EQUIP_TURTLE, 1.0F, 1.0F);
+                }
+                else if (Item.getType().toString().toLowerCase().contains("chestplate")) {
+                    ItemStack returnItem = player.getInventory().getChestplate();
+                    player.getInventory().setItemInMainHand(returnItem);
+                    player.getInventory().setChestplate(Item);
+                    player.playSound(player.getLocation(), Sound.ITEM_ARMOR_EQUIP_TURTLE, 1.0F, 1.0F);
+                }
+                else if (Item.getType().toString().toLowerCase().contains("boots")) {
+                    ItemStack returnItem = player.getInventory().getBoots();
+                    player.getInventory().setItemInMainHand(returnItem);
+                    player.getInventory().setBoots(Item);
+                    player.playSound(player.getLocation(), Sound.ITEM_ARMOR_EQUIP_TURTLE, 1.0F, 1.0F);
+                }
+                else if (Item.getType().toString().toLowerCase().contains("leggings")) {
+                    ItemStack returnItem = player.getInventory().getLeggings();
+                    player.getInventory().setItemInMainHand(returnItem);
+                    player.getInventory().setLeggings(Item);
+                    player.playSound(player.getLocation(), Sound.ITEM_ARMOR_EQUIP_TURTLE, 1.0F, 1.0F);
+                }
+                else {
+                    player.sendMessage(ChatColor.AQUA + "ItemInHand " + Item);
+                }
+            }
+
+        }
+    }
 }
